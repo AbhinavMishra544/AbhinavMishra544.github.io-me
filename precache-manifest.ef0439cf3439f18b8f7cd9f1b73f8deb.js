bb31aa02e4b254aae6c8d550717e7b2d236fd0b7
abhinav <abhinavmishra544@gmail.com>
2020-11-07 23:34:36 +0530

    Updates

---
+++ precache-manifest.ef0439cf3439f18b8f7cd9f1b73f8deb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aab7ca4067b4ab6226ad",
+    "url": "/me/static/css/main.3416d55f.chunk.css"
+  },
+  {
+    "revision": "aab7ca4067b4ab6226ad",
+    "url": "/me/static/js/main.aab7ca40.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "d52d05026ce8781719151cd542140750",
+    "url": "/me/index.html"
+  }
+];
